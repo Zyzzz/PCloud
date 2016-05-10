6139dcc72035755065c03a9807c68b664f235e68
zyzzz <zhouyan@imudges.com>
2016-05-10 15:28:29 +0800

    GetUserSharingList

---
+++ PCloudServer/src/imu/pcloud/server/action/GetUserSharingList.java
@@ -0,0 +1,44 @@
+package imu.pcloud.server.action;
+
+import imu.pcloud.server.model.BaseModel;
+import imu.pcloud.server.model.UserSharingList;
+import imu.pcloud.server.service.SharingRecordService;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class GetUserSharingList extends ActionSupport {
+	
+	SharingRecordService sharingRecordService = new SharingRecordService();
+	String cookies;
+	UserSharingList result = new UserSharingList();
+	public String getCookies() {
+		return cookies;
+	}
+	public void setCookies(String cookies) {
+		this.cookies = cookies;
+	}
+
+	public UserSharingList getResult() {
+		return result;
+	}
+	
+	public void setResult(UserSharingList result) {
+		this.result = result;
+	}
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		int statc = sharingRecordService.getUserSharingList(cookies); 
+		if(statc==0){
+			result.setStatus(303);
+			result.setSharingRecords(sharingRecordService.getSharingRecords());
+		}
+		else {
+			result.setStatus(300);
+		}
+		return SUCCESS;
+	}
+	
+	
+	
+}
