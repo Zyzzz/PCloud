ce75c1f77346f236e73e8bcd5dd7ff19ef358822
zyzzz <zhouyan@imudges.com>
2016-06-04 19:50:50 +0800

    getMultiPlanListByUserIdAction

--- PCloudServer/src/imu/pcloud/server/action/getMultiPlanListByUserIdAction.java
+++ PCloudServer/src/imu/pcloud/server/action/getMultiPlanListByUserIdAction.java
@@ -1,51 +1,47 @@
 package imu.pcloud.server.action;
 
-import imu.pcloud.server.model.BaseModel;
+import imu.pcloud.server.model.MultiPlanList;
 import imu.pcloud.server.service.MultiPlanService;
 import imu.pcloud.server.service.UserService;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class JoinMultiPlanAction extends ActionSupport {
-
+public class getMultiPlanListByUserIdAction extends ActionSupport {
 	String cookies;
-	Integer multiPlanId;
-	BaseModel result = new BaseModel();
+	MultiPlanList result = new MultiPlanList();
 	MultiPlanService multiPlanService = new MultiPlanService();
-	
 	public String getCookies() {
 		return cookies;
 	}
 	public void setCookies(String cookies) {
 		this.cookies = cookies;
 	}
-	public Integer getMultiPlanId() {
-		return multiPlanId;
-	}
-	public void setMultiPlanId(Integer multiPlanId) {
-		this.multiPlanId = multiPlanId;
-	}
-	public BaseModel getResult() {
+	public MultiPlanList getResult() {
 		return result;
 	}
-	public void setResult(BaseModel result) {
+	public void setResult(MultiPlanList result) {
 		this.result = result;
 	}
 	@Override
 	public String execute() throws Exception {
 		// TODO Auto-generated method stub
-		
 		UserService userService = new UserService();
 		int statc = userService.reLogin(cookies);
 		if(statc==0){
-			statc = multiPlanService.joinMultiPlan(userService.getUser().getId(), multiPlanId);
-			result.setStatus(statc);
+			 statc = multiPlanService.getMultiPlanListByUserId(userService.getUser().getId());
+			 if(statc == 0){
+				 result.setMultiPlanMembers(multiPlanService.getMultiPlanMembers());
+				 result.setMultiPlans(multiPlanService.getMultiPlans());
+				 result.setStatus(0);
+			 }else {
+				result.setStatus(statc);
+			}
+			 
 		}else {
 			result.setStatus(104);
 		}
 		return SUCCESS;
 	}
-	
 	
 	
 }
